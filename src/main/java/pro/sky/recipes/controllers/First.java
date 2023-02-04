0bc894c1b63ae7f715c77f7a231e39f1667308ae
Мария <mariya_piterskaya@ymail.com>
2023-02-04 23:14:32 +0300

    done

--- src/main/java/pro/sky/recipes/controllers/First.java
+++
@@ -1,4 +0,0 @@
-package pro.sky.recipes.controllers;
-
-public class First {
-}
---
+++ src/main/java/pro/sky/recipes/controllers/FirstController.java
@@ -0,0 +1,17 @@
+package pro.sky.recipes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping
+    public String applicationRun(){
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "Машенька. Рецепты. 04.02.2023. Сборник рецептов.";
+    }
+}
